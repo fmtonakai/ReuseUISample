2236c15e125cba3e1d12592a6d0a322cfbeb85a0
Masaki Fuke <masaki.fuke@excite.jp>
2014-04-08 20:26:52 +0900

    同じUIを何個も作る

---
+++ ReuseUISample/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  ReuseUISample
+//
+//  Created by masaki.fuke on 2014/04/08.
+//  Copyright (c) 2014年 masaki.fuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
